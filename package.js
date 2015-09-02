781745d12c240a75398e9edc1775fca8b0b4d1a2
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2015-09-02 17:40:11 +0300

    Implement Bitly OAuth

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ var fs = Npm.require("fs");
 eval(fs.readFileSync("./packages/autopackage.js").toString());
 Package.onUse(function(api) {
   addFiles(api, description.name, getDefaultProfiles());
-  api.use(["foundation@1.0.0"]);
+  api.use(["foundation"]);
   api.use([
     "percolate:migrations@0.7.3"
   ]);
